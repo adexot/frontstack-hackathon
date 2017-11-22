06390d4acdc0566af030dc242b77ebf38edca287
adexot <adekunleoseni47@gmail.com>
2017-11-22 18:14:11 +0100

    created the desktop page

---
+++ js/main.js
@@ -0,0 +1,30 @@
+(function(){
+  
+  
+  window.addEventListener('scroll', (e) => {
+    let navHeight = document.querySelector('.navbar').clientHeight;
+    if(window.scrollY > navHeight && window.innerWidth < 720){
+      document.querySelector('.navbar').classList.add("navbar-fixed");
+      document.querySelector('.navbar-btn').style.display = "none";
+      document.querySelector('.mobile').style.display = "inline-block";
+      e.stopPropagation();
+    }
+    else{
+      document.querySelector('.navbar').classList.remove("navbar-fixed");
+      document.querySelector('.navbar-btn').style.display = "inline";
+      document.querySelector('.mobile').style.display = "none";
+    }
+  });
+
+  let mobile = false;
+  document.querySelector('.mobile').addEventListener('click', (e) => {
+    if(!mobile){
+      document.querySelector('.mobile-menu').style.display = "block";
+      mobile = true;
+      e.stopPropagation();
+    }else{
+      document.querySelector('.mobile-menu').style.display = "none";
+      mobile = false;
+    }
+  });
+})();
